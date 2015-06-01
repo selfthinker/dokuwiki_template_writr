674788542209cc3cc9ee9c3b304d121779190272
Anika Henke <anika@selfthinker.org>
2015-06-01 02:05:40 +0100

    whitespace fixes

--- js/navigation.js
+++ js/navigation.js
@@ -4,31 +4,31 @@
  * Handles toggling the navigation menu for small screens.
  */
 jQuery(function(){
-	var container, button, menu;
+    var container, button, menu;
 
-	container = document.getElementById( 'writr__site-navigation' );
-	if ( ! container )
-		return;
+    container = document.getElementById( 'writr__site-navigation' );
+    if ( ! container )
+        return;
 
-	button = container.getElementsByTagName( 'h1' )[0];
-	if ( 'undefined' === typeof button )
-		return;
+    button = container.getElementsByTagName( 'h1' )[0];
+    if ( 'undefined' === typeof button )
+        return;
 
-	menu = container.getElementsByTagName( 'ul' )[0];
+    menu = container.getElementsByTagName( 'ul' )[0];
 
-	// Hide menu toggle button if menu is empty and return early.
-	if ( 'undefined' === typeof menu ) {
-		button.style.display = 'none';
-		return;
-	}
+    // Hide menu toggle button if menu is empty and return early.
+    if ( 'undefined' === typeof menu ) {
+        button.style.display = 'none';
+        return;
+    }
 
-	if ( -1 === menu.className.indexOf( 'nav-menu' ) )
-		menu.className += ' nav-menu';
+    if ( -1 === menu.className.indexOf( 'nav-menu' ) )
+        menu.className += ' nav-menu';
 
-	button.onclick = function() {
-		if ( -1 !== container.className.indexOf( 'toggled' ) )
-			container.className = container.className.replace( ' toggled', '' );
-		else
-			container.className += ' toggled';
-	};
+    button.onclick = function() {
+        if ( -1 !== container.className.indexOf( 'toggled' ) )
+            container.className = container.className.replace( ' toggled', '' );
+        else
+            container.className += ' toggled';
+    };
 });
