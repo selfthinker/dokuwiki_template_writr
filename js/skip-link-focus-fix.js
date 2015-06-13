7dda8de7952c2e98dfdb47665c6657c670f376c1
Anika Henke <anika@selfthinker.org>
2015-06-13 22:40:39 +0100

    consolidated most js into one, rewrote navigation toggle and skip link focus js, close toc by default

--- js/skip-link-focus-fix.js
+++ js/skip-link-focus-fix.js
@@ -1,19 +1,36 @@
-( function() {
-    var is_webkit = navigator.userAgent.toLowerCase().indexOf( 'webkit' ) > -1,
-        is_opera  = navigator.userAgent.toLowerCase().indexOf( 'opera' )  > -1,
-        is_ie     = navigator.userAgent.toLowerCase().indexOf( 'msie' )   > -1;
+(function($) {
 
-    if ( ( is_webkit || is_opera || is_ie ) && 'undefined' !== typeof( document.getElementById ) ) {
-        var eventMethod = ( window.addEventListener ) ? 'addEventListener' : 'attachEvent';
-        window[ eventMethod ]( 'hashchange', function() {
-            var element = document.getElementById( location.hash.substring( 1 ) );
-
-            if ( element ) {
-                if ( ! /^(?:a|select|input|button|textarea)$/i.test( element.tagName ) )
-                    element.tabIndex = -1;
-
-                element.focus();
-            }
-        }, false );
+  jQuery.extend(jQuery.expr[':'], {
+    focusable: function(el, index, selector){
+      var $element = $(el);
+      return $element.is(':input:enabled, a[href], area[href], object, [tabindex]') && !$element.is(':hidden');
     }
-})();
+  });
+
+  function focusOnElement($element) {
+    if (!$element.length) {
+      return;
+    }
+    if (!$element.is(':focusable')) {
+      // add tabindex to make focusable and remove again
+      $element.attr('tabindex', -1).on('blur focusout', function () {
+        $(this).removeAttr('tabindex');
+      });
+    }
+    $element.focus();
+  }
+
+  $(document).ready(function(){
+    // if there is a '#' in the URL (someone linking directly to a page with an anchor)
+    if (document.location.hash) {
+      focusOnElement($(document.location.hash));
+    }
+
+    // if the hash has been changed (activation of an in-page link)
+    $(window).bind('hashchange', function() {
+      var hash = "#"+window.location.hash.replace(/^#/,'');
+      focusOnElement($(hash));
+    });
+  });
+
+})(jQuery);
--- script.js
+++ script.js
@@ -1,3 +1,79 @@
-/* DOKUWIKI:include js/navigation.js */
 /* DOKUWIKI:include js/skip-link-focus-fix.js */
-/* DOKUWIKI:include js/writr.js */
+
+( function( $ ) {
+    /*
+     * Click to toggle sidebar.
+     */
+    function toggleSidebar() {
+        $( '#writr__sidebar' ).on( 'click', '#writr__sidebar-toggle', function( e ) {
+            e.preventDefault();
+            $( 'html, body' ).scrollTop( 0 );
+            $( this ).toggleClass( 'open' );
+            $( 'body' ).toggleClass( 'sidebar-closed' );
+            $( '#writr__secondary' ).resize();
+        } );
+    }
+
+    /**
+     * Handles toggling the navigation menu for small screens.
+     */
+    function toggleNavigation() {
+        var $container = $('#writr__site-navigation');
+        if (!$container.length) return;
+        var $button = $('.menu-toggle', $container);
+        if (!$button.length) return;
+        var $menu = $('ul', $container);
+        if (!$menu.length) {
+            $menu.hide();
+            return;
+        }
+        $button.click(function(){
+            $container.toggleClass('toggled');
+        });
+    }
+
+    /*
+     * A function to enable/disable a dropdown submenu.
+     */
+    function toggleSubmenu() {
+        $( '.main-navigation .node > div > a' ).append( '<span class="dropdown-icon" />' );
+        $( '#writr__site-navigation' ).on( 'click', '.dropdown-icon', function( e ) {
+            e.preventDefault();
+            $( this ).toggleClass( 'open' );
+            if ( $( this ).hasClass( 'open' ) ) {
+                $( this ).parent().parent().next( 'ul' ).show();
+            } else {
+                $( this ).parent().parent().next( 'ul' ).hide();
+            }
+        } );
+    }
+
+    /*
+     * Close TOC by default
+     */
+    function closeToc() {
+        var $toc = jQuery('#dw__toc .toggle');
+        if($toc.length) {
+            $toc[0].setState(-1);
+        }
+    }
+
+    /*
+     * Change search submit input to submit button to make it easier to style
+     */
+    function changeSearchInput() {
+        var $searchForm = $('.search-form > form > div');
+        var $searchButton = $('input[type="submit"]', $searchForm).detach();
+        var title = $searchButton.attr('title');
+        var value = $searchButton.val();
+        $searchForm.append('<button type="submit" title="'+title+'">'+value+'</button>');
+    }
+
+    $(function(){
+        toggleSidebar();
+        toggleNavigation();
+        toggleSubmenu();
+        closeToc();
+        changeSearchInput();
+    });
+} )( jQuery );
