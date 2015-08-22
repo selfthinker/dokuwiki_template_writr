787ebf2b34f565800837cfcd285970a421c8195f
Anika Henke <anika@selfthinker.org>
2015-08-22 14:56:38 +0100

    updates for use with styling plugin

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -5,3 +5,6 @@
  */
 
 $lang['menu'] = 'Menu';
+$lang['__theme_color__'] = 'Primary theme colour';
+$lang['__theme_color_alt__'] = 'Secondary theme colour';
+$lang['__content_width__'] = 'Width of the content area';
