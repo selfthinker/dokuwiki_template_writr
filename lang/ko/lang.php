f686122c57ac0cf2cd5acd3c88bf740215fe8b33
Myeongjin <aranet100@gmail.com>
2016-04-22 14:59:20 +0900

    translation update

---
+++ lang/ko/lang.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Korean language file for template
+ *
+ */
+
+$lang['menu'] = '메뉴';
+$lang['__theme_color__'] = '첫째 테마 색';
+$lang['__theme_color_alt__'] = '둘째 테마 색';
+$lang['__content_width__'] = '내용 영역의 너비';
