5c46d2ca840edc093ca689b61c6fd6a0c5982e2d
Louis Ouellet <louis_ouellet@hotmail.com>
2024-01-02 12:35:08 -0500

    Updated English Translation and added French Translation

---
+++ lang/fr/lang.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Language file for template
+ *
+ */
+
+$lang['menu'] = 'Menu';
+$lang['__theme_color__'] = 'Couleur principale du thème';
+$lang['__theme_color_alt__'] = 'Couleur secondaire du thème';
+$lang['__content_width__'] = 'Largeur de la zone de contenu';
+$lang['AddNewPage'] = 'Ajouter une nouvelle page';
+$lang['Language'] = 'Langue';
---
+++ lang/fr/settings.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Language File for Configuration
+ *
+ * @author   Louis Ouellet <louis_ouellet@hotmail.com>
+ * @license  GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ */
+
+$lang['showPageToolsTitle']                 = 'Afficher le titre des outils de la page';
+$lang['showSiteToolsTitle']                 = 'Afficher le titre des outils du site';
+$lang['showUserToolsTitle']                 = 'Afficher le titre des outils utilisateur';
+
+$lang['doSiteToolsRequireLogin']            = 'Les outils du site doivent-ils nécessiter une connexion';
+$lang['doLogoChangesByNamespace']           = 'Les changements de logo doivent-ils être par espace de noms';
+
+$lang['defaultAddNewPage']                  = 'Namespace pour l\'extension AddNewPage';
+
+$lang['font']                               = 'Police';
