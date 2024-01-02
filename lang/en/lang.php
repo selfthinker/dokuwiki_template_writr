1d434895c32b4eeae406ad9f70baf2b3bca7d960
Louis Ouellet <louis_ouellet@hotmail.com>
2024-01-02 12:07:17 -0500

    Updated Language Files

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -8,3 +8,5 @@ $lang['menu'] = 'Menu';
 $lang['__theme_color__'] = 'Primary theme colour';
 $lang['__theme_color_alt__'] = 'Secondary theme colour';
 $lang['__content_width__'] = 'Width of the content area';
+$lang['AddNewPage'] = 'Add new page';
+$lang['Language'] = 'Language';
