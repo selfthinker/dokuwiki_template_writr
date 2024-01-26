8cff255124eb487efa39b83563ac4f20696fe53a
Louis Ouellet <louis_ouellet@hotmail.com>
2024-01-26 12:55:18 -0500

    Added placeholders for the korean language

---
+++ lang/ko/settings.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Language File for Configuration
+ *
+ * @author   Louis Ouellet <louis_ouellet@hotmail.com>
+ * @license  GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ */
+
+$lang['_tools']                             = 'Configure Tools';
+$lang['showPageToolsTitle']                 = 'Show the title of the page tools';
+$lang['showSiteToolsTitle']                 = 'Show the title of the site tools';
+$lang['showUserToolsTitle']                 = 'Show the title of the user tools';
+$lang['doSiteToolsRequireLogin']            = 'Should the site tools require login';
+
+$lang['_extra']                             = 'Configure Extras';
+$lang['doLogoChangesByNamespace']           = 'Should the logo changes be by namespace';
+
+$lang['_plugins']                           = 'Configure Plugins';
+$lang['defaultAddNewPage']                  = 'Namespace for AddNewPage plugin';
+
+$lang['_customizations']                    = 'Customize the Template';
