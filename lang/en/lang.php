49bf5335cb90d1f9e4918fe99f4f98a655b96aba
Louis Ouellet <louis_ouellet@hotmail.com>
2024-03-07 16:30:09 -0500

    FEATURE - Added a custom color for the user tools area. Available in template style configurations

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -8,6 +8,7 @@ $lang['menu']                               = 'Menu';
 $lang['__tooltips_background_color__']      = 'Background colour of tooltips';
 $lang['__tooltips_text_color__']            = 'Text colour of tooltips';
 $lang['__tooltips_border_color__']          = 'Border colour of tooltips';
+$lang['__sidebar_user_tools_color__']       = 'Background colour of the user tools';
 $lang['__theme_color__']                    = 'Primary theme colour';
 $lang['__theme_color_alt__']                = 'Secondary theme colour';
 $lang['__content_width__']                  = 'Width of the content area';
--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -8,6 +8,7 @@ $lang['menu']                               = 'Menu';
 $lang['__tooltips_background_color__']      = 'Couleur arrière-plan des infos-bulles';
 $lang['__tooltips_text_color__']            = 'Couleur text des infos-bulles';
 $lang['__tooltips_border_color__']          = 'Couleur bordure des infos-bulles';
+$lang['__sidebar_user_tools_color__']       = 'Couleur arrière-plan des outils utilisateur';
 $lang['__theme_color__']                    = 'Couleur principale du thème';
 $lang['__theme_color_alt__']                = 'Couleur secondaire du thème';
 $lang['__content_width__']                  = 'Largeur de la zone de contenu';
